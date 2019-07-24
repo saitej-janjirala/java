c46304bd666f6158e038214c255f9f5b90614e5a
saiteja-janjirala <45119629+saitej-janjirala@users.noreply.github.com>
2019-07-24 23:31:26 +0530

    walkie talkie  chat between client and server

---
+++ client server communication/Server.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.net.*;
+import java.io.*;
+class Server
+{
+	public static void main(String args[])throws Exception
+	{
+		ServerSocket server=new ServerSocket(1234);
+		Socket s=server.accept();
+		System.out.println("connection established");
+		DataInputStream dis=new DataInputStream(s.getInputStream());
+		DataOutputStream dos=new DataOutputStream(s.getOutputStream());
+		Scanner S=new Scanner(System.in);
+		
+		while(true)
+		{
+			String out=S.nextLine();
+			
+			if(out.equals("over"))
+			{
+				dos.writeUTF(out);
+				break;
+			}
+			else
+			{
+				dos.writeUTF(out);
+			}
+			String in=dis.readUTF();
+			
+			if(in.equals("over"))
+			{
+				System.out.println("client:"+in);
+				break;
+			}
+			else
+			{
+				System.out.println("client:"+in);
+			}
+		}
+		
+		
+		
+		
+		
+		dis.close();
+		dos.close();
+		s.close();
+		server.close();
+	}
+}
